413bf27c8a62bb81fd01f3a1742d993a490c98d0
Aya Adel <aya.adel.wd@gmail.com>
2020-12-26 23:13:07 +0200

    finish week page

---
+++ assets/src/scripts/script.js
@@ -0,0 +1,6 @@
+const breadcrumbMenu = document.getElementById('breadcrumbMenu');
+
+const hideMenu = () => {
+  breadcrumbMenu.classList.remove('show');
+};
+const displayMenu = () => breadcrumbMenu.classList.add('show');
