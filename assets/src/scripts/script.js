7e7ca2ee67fe84629e700bf11f2dba58c5a6b368
Aya Adel <aya.adel.wd@gmail.com>
2021-01-06 15:13:40 +0200

    finish homepage

--- assets/src/scripts/script.js
+++ assets/src/scripts/script.js
@@ -4,3 +4,7 @@ const hideMenu = () => {
   breadcrumbMenu.classList.remove('show');
 };
 const displayMenu = () => breadcrumbMenu.classList.add('show');
+
+$(document).ready(function () {
+  $('.js-example-basic-single').select2();
+});
