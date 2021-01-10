8a7ce44010a170070698f146e1642d518cfcc977
Aya Adel <aya.adel.wd@gmail.com>
2021-01-10 21:43:28 +0200

    finish home page

--- assets/src/scripts/script.js
+++ assets/src/scripts/script.js
@@ -7,4 +7,33 @@ const displayMenu = () => breadcrumbMenu.classList.add('show');
 
 $(document).ready(function () {
   $('.js-example-basic-single').select2();
+
+  $('.slideshow').slick({
+    arrows: true,
+    prevArrow: '<i class="fa fa-angle-up"></i>',
+    nextArrow: '<i class="fa fa-angle-down"></i>',
+    dots: true,
+    dotsClass: 'slick-dots',
+    vertical: true,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+  });
+
+  $('.teams-slider').slick({
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 1,
+    dots: true,
+    dotsClass: 'slick-dots',
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+        },
+      },
+    ],
+  });
 });
